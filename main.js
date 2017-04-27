d7e794410a245996eb8c19d500281304dc62e0a7
lasperilla <l.asperilla@gmail.com>
2017-04-27 03:07:53 -0400

    fixed game with recursive. wrote cloze func. added new game prompt and play again prompt

--- main.js
+++ main.js
@@ -3,43 +3,121 @@ var basic = require("./basic.json");
 var cloze = require("./cloze.json");
 
 var score = 0;
+var count = 0;
+var mode;
+
+function done() {
+    if (mode === "basic") {
+        var length = basic.length;
+    } else {
+        var length = cloze.length;
+    }
+    console.log("");
+    console.log("=====================");
+    console.log("All done.");
+    console.log("Correct Answers:" + score);
+    console.log("Inorrect Answers:" + (length - score));
+    console.log("=====================");
+    console.log("");
+
+    inquirer.prompt([{
+    	type: "list",
+    	message: "Play again?",
+    	choices: ["Yes", "No"],
+    	name: "userInput"
+    }]).then(function(answers){
+    	if (answers.userInput === "Yes") {
+    		score = 0;
+    		count = 0;
+    		newGame();
+    	} else {
+    		console.log("Thanks for playing.")
+    	};
+    })
+}
 
 class card {
-    constructor(basic) {
-        this.front = basic.front;
-        this.back = basic.back;
-    }
-    prompt() {
-    		var that = this;
-        inquirer.prompt([{
-            name: "userInput",
-            message: this.front
-        }]).then(function(answers) {
-            this.checkAnswers(answers);
-        });
-    }
-    checkAnswers(answers) {
-        if (answers.userInput.toLowerCase() === that.back) {
-            score++;
-            console.log("Correct");
-            console.log("Score: " + score);
-        } else {
-            console.log("Incorrect");
-            console.log("Score: " + score);
-        }
+    constructor(card) {
+        this.front = card.front;
+        this.back = card.back;
     }
 };
 
-// console.log(basic[0].back);
+class clozeCard extends card {
+	constructor(card) {
+        super(card);
+        this.cloze = card.cloze;
+        this.partial = card.front.replace(this.cloze, "...")
+    }
+}
 
-// for (var i = 0; i < basic.length; i++) {
-//     var flashcard = new card(basic[i]);
-//     // console.log (flashcard);
-//     flashcard.prompt();
-// }; //end for loop
+function basicPrompt() {
+    if (count < basic.length) {
+        var flashcard = new card(basic[count]);
+        inquirer.prompt([{
+            name: "userInput",
+            message: flashcard.front
+        }]).then(function(answers) {
+            if (answers.userInput.toLowerCase() === flashcard.back) {
+                score++;
+                console.log("Correct");
+                console.log("Score: " + score);
+            } else {
+                console.log("Incorrect");
+                console.log("Correct Answer: " + flashcard.back)
+                console.log("Score: " + score);
+            };
 
-basic.forEach(function(element){
-	  var flashcard = new card(element);
-    flashcard.prompt();
-   // console.log(flashcard)
-});
+            count++;
+            basicPrompt();
+        });
+    } else {
+        done();
+    };
+};
+
+function clozePrompt() {
+    if (count < cloze.length) {
+        var flashcard = new clozeCard(cloze[count]);
+        inquirer.prompt([{
+            name: "userInput",
+            message: flashcard.partial
+        }]).then(function(answers) {
+            if (answers.userInput.toLowerCase() === flashcard.cloze.toLowerCase()) {
+                score++;
+                console.log("Correct");
+                console.log("Score: " + score);
+            } else {
+                console.log("Incorrect");
+                console.log("Correct Answer: " + flashcard.front)
+                console.log("Score: " + score);
+            };
+
+            count++;
+            clozePrompt();
+        });
+    } else {
+        done();
+    };
+};
+
+function newGame() {
+    inquirer.prompt([{
+        type: "list",
+        message: "Choose flashcard type",
+        choices: ["Basic", "Cloze", ">Exit"],
+        name: "userChoice"
+    }]).then(function(answers) {
+        if(answers.userChoice === "Basic") {
+            mode = "basic";
+            basicPrompt();
+        } else if(answers.userChoice === "Cloze") {
+            mode = "cloze"
+            clozePrompt();
+        } else {
+        	console.log("Goodbye.");
+        };
+    });
+};
+
+newGame();
